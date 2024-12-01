bb59f438eaff443bdc9313f79605768dc5a3ce23
mukk45 <mukeshmukku2006@gmail.com>
2024-12-01 21:48:55 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,13 @@
+document.getElementById('registrationForm').addEventListener('submit', function(e) {
+    e.preventDefault();
+  
+    const password = document.getElementById('password').value;
+    const confirmPassword = document.getElementById('confirmPassword').value;
+  
+    if (password !== confirmPassword) {
+      alert('Passwords do not match!');
+      return;
+    }
+  
+    alert('Registration successful!');
+  });
